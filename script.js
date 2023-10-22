c1ecb62a08e3dc9b85cda9819d85e7518831ff2a
Md Tanvir Hossain <118459299+ewtanvir@users.noreply.github.com>
2023-10-23 00:34:12 +0600

    java

---
+++ script.js
@@ -0,0 +1,22 @@
+// Define your key-to-link mapping here
+function getLinkForKey(key) {
+    // Example key-to-link mapping
+    var keyToLink = {
+        "key1": "https://example.com/link1",
+        "key2": "https://examndmdmvple.com/link2",
+        "vvn": "https://fgbd.com/link2",
+        "xnks": "https://exdbvxcample.com/link2",
+        "skdk": "https://dg.com/link2",
+        "mdmdk": "https://edggdgxample.com/link2",
+        "mmm": "https://dgdg  dg.com/link2",
+        "MMM": "https://dgdgdg.com/link2",
+        "mdkdmKSKSS": "https://dgdg.com/link2",
+        "S": "https://example.dgdgdgd/link2",
+        "101": "https://exampdgdgle.com/link2",
+        "MmM": "https://exadgdgdgmple.com/link2",
+        // Add more key-to-link mappings as needed
+    };
+
+    return keyToLink[key] || null;
+}
+
