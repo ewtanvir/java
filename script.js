eef827bc82a20f09a3e138575598024f264e1063
Md Tanvir Hossain <118459299+ewtanvir@users.noreply.github.com>
2023-10-24 23:41:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 function getLinkForKey(key) {
     // Example key-to-link mapping
     var keyToLink = {
-        "key1": "https://example.com/link1",
+        "key1": "https://cutt.ly/all-viral-link-2023",
         "key2": "https://examndmdmvple.com/link2",
         "vvn": "https://fgbd.com/link2",
         "xnks": "https://exdbvxcample.com/link2",
         "skdk": "https://dg.com/link2",
         "mdmdk": "https://edggdgxample.com/link2",
-        "mmm": "https://dgdg fmffmfmfmfmfmf  dg.com/link2",
+        "mmm": "https://dgdg  dg.com/link2",
         "MMM": "https://dgdgdg.com/link2",
-        "#": "https://dgdg.com/link2",
-        "S": "#",
+        "mdkdmKSKSS": "https://dgdg.com/link2",
+        "S": "https://example.dgdgdgd/link2",
         "101": "https://exampdgdgle.com/link2",
         "MmM": "https://exadgdgdgmple.com/link2",
-        "test": "https://#.com/link2",
-        
-
-        
         // Add more key-to-link mappings as needed
     };
 
     return keyToLink[key] || null;
+}
+
+function generateLink() {
+    var key = document.getElementById("keyInput").value;
+    var link = getLinkForKey(key);
+    document.getElementById("linkResult").innerHTML = link ? `<a class="button" href="${link}" target="_blank">${link}</a>` : "Invalid key.";
+    
 }
 
